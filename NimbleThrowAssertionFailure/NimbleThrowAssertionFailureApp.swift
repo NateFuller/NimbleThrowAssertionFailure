d11eccf9e928cbd1b012b54c765c5acc548703fe
Nathan Fuller <nathan.fuller@grubhub.com>
2022-02-22 13:50:55 -0500

    Initial Commit

---
+++ NimbleThrowAssertionFailure/NimbleThrowAssertionFailureApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbleThrowAssertionFailureApp.swift
+//  NimbleThrowAssertionFailure
+//
+//  Created by Nathan Fuller on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct NimbleThrowAssertionFailureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
